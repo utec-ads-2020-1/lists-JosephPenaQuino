706fb68a73f6f0abd95031a431ebadecd552152a
Joseph <joseph.pena@utec.edu.pe>
2020-04-26 19:41:08 -0500

    Add forward_iterator solution

--- structures/lists/iterators/forward_iterator.h
+++ structures/lists/iterators/forward_iterator.h
@@ -2,13 +2,15 @@
 #define FORWARD_ITERATOR_H
 
 #include "../node.h"
+#include "../forward.h"
 
-// TODO: Implement all methods
+// TODO: Add functionality to add -- from end()
 template <class T> 
 class ForwardIterator {
     private:
         Node<T> *current;
 
+        template<class B> friend class ForwardList;
     public:
         ForwardIterator();
         ForwardIterator(Node<T>*);
@@ -16,45 +18,47 @@ class ForwardIterator {
         ForwardIterator<T> operator=(ForwardIterator<T>);
 
         bool operator!=(ForwardIterator<T>);
+        bool operator==(ForwardIterator<T>);
 
         ForwardIterator<T> operator++();
 
         T operator*();
 };
 template <class T> 
-ForwardIterator<T>::ForwardIterator()
-{
+ForwardIterator<T>::ForwardIterator() : current{nullptr} {}
 
+template <class T> 
+ForwardIterator<T>::ForwardIterator(Node<T>* node) : current{node} {}
+
+template <class T> 
+ForwardIterator<T> ForwardIterator<T>::operator=(ForwardIterator<T> it)
+{
+    current = it.current;
+    return *this;
 }
 
 template <class T> 
-ForwardIterator<T>::ForwardIterator(Node<T>*)
+bool ForwardIterator<T>::operator!=(ForwardIterator<T> it)
 {
-
+    return this->current != it.current;
 }
 
 template <class T> 
-ForwardIterator<T> ForwardIterator<T>::operator=(ForwardIterator<T>)
+bool ForwardIterator<T>::operator==(ForwardIterator<T> it)
 {
-
-}
-
-template <class T> 
-bool ForwardIterator<T>::operator!=(ForwardIterator<T>)
-{
-
+    return current == it.current;
 }
 
 template <class T> 
 ForwardIterator<T> ForwardIterator<T>::operator++()
 {
-
+    current = current->next;
 }
 
 template <class T> 
 T ForwardIterator<T>::operator*()
 {
-
+    return current->data;
 }
 
 #endif
